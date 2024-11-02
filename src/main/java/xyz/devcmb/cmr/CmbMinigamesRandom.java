2cf61f241528f73e800c431cbed616c2d9dff1c0
DevCmb <71194682+29cmb@users.noreply.github.com>
2024-11-02 14:01:31 -0400

    Initial Commit

---
+++ src/main/java/xyz/devcmb/cmr/CmbMinigamesRandom.java
@@ -0,0 +1,17 @@
+package xyz.devcmb.cmr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CmbMinigamesRandom extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
