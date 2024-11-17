995c42dd4c5378821da78c0f0a97f5aa762291be
Cmb <71194682+29cmb@users.noreply.github.com>
2024-11-17 09:10:28 -0500

    Make the music box display the author using a bossbar instead of an achievement

--- src/main/java/xyz/devcmb/cmr/utils/MusicBox.java
+++ src/main/java/xyz/devcmb/cmr/utils/MusicBox.java
@@ -1,8 +1,11 @@
 package xyz.devcmb.cmr.utils;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.SoundCategory;
+import org.bukkit.scheduler.BukkitRunnable;
 import xyz.devcmb.cmr.CmbMinigamesRandom;
 
 import java.util.HashMap;
@@ -37,7 +40,31 @@ public class MusicBox {
 
         Bukkit.getOnlinePlayers().forEach(player -> {
             player.playSound(player, track.get("path"), SoundCategory.MUSIC, 1, 1);
-            Utilities.showAdvancement(player, "minecraft:jukebox", "Now Playing", "\"" + track.get("song") + "\" by " + track.get("author"));
+//            Utilities.showAdvancement(player, "minecraft:jukebox", "Now Playing", "\"" + track.get("song") + "\" by " + track.get("author"));
+            Audience audience = CmbMinigamesRandom.adventure().player(player);
+            final BossBar bossBar = BossBar.bossBar(
+                    Component.text("\"" + track.get("song") + "\" by " + track.get("author")),
+                    0,
+                    BossBar.Color.BLUE,
+                    BossBar.Overlay.PROGRESS
+            );
+
+            audience.showBossBar(bossBar);
+            new BukkitRunnable() {
+                double progress = 0;
+                final double increment = 1.0 / 100;
+
+                @Override
+                public void run() {
+                    if (progress >= 1) {
+                        audience.hideBossBar(bossBar);
+                        this.cancel();
+                    } else {
+                        progress += increment;
+                        bossBar.progress((float) progress);
+                    }
+                }
+            }.runTaskTimer(CmbMinigamesRandom.getPlugin(), 0, 1);
         });
     }
 }
