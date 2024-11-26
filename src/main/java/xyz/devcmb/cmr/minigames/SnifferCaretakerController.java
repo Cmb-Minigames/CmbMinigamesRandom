312ef75506cb1bb9e7d4b405368713133e1bd352
Cmb <71194682+29cmb@users.noreply.github.com>
2024-11-25 19:05:34 -0500

    Add re-usable gameStart to sniffer caretaker

--- src/main/java/xyz/devcmb/cmr/minigames/SnifferCaretakerController.java
+++ src/main/java/xyz/devcmb/cmr/minigames/SnifferCaretakerController.java
@@ -100,6 +100,7 @@ public class SnifferCaretakerController implements Minigame {
     @SuppressWarnings("unchecked")
     @Override
     public void start() {
+        Utilities.gameStartReusable();
         List<Player> allPlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
         Collections.shuffle(allPlayers);
 
